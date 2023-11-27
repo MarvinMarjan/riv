9f104908bf906b46693c031caac3c33ef3027a53
Marvin Marjan <marvin.marjan@gmail.com>
2023-11-27 10:01:34 -0400

    add comments in type.cpp; split functions into small pieces.

--- src/type/type.cpp
+++ src/type/type.cpp
@@ -4,46 +4,66 @@
 
 
 
+// searches the '.' index in a double value string representation
+static size_t get_doublestr_decimal_dot_index(const std::string& strdouble) noexcept
+{
+	size_t decimal_begin = 0;
+
+	for (size_t i = 0; i < strdouble.size(); i++)
+		if (strdouble[i] == '.')
+		{
+			decimal_begin = i;
+			break;
+		}
+
+	return decimal_begin;
+}
+
+
+// erase irrelevant zeros
+static void erase_doublestr_decimal(std::string& strdouble, const size_t decimal_begin, const size_t last_relevant_decimal) noexcept
+{
+	// is last relevant decimal index the same as the '.' index?
+	if (last_relevant_decimal == decimal_begin)
+		strdouble.erase(last_relevant_decimal); // erase everything starting from '.'
+	else
+		strdouble.erase(last_relevant_decimal + 1); // erase everything after the last relevant decimal index
+}
+
+
+// remove irrelevant zeros from a double converted to string.
+// if "3.14000" is passed, the return will be "3.14"
+static std::string trim_irrelevant_doublestr_zeros(const std::string& strdouble) noexcept
+{
+	std::string trimmed = strdouble;
+
+	// index of '.'
+	size_t decimal_begin = get_doublestr_decimal_dot_index(strdouble);
+	
+	// last decimal number different from zero
+	size_t last_relevant_decimal = decimal_begin;
+
+	erase_doublestr_decimal(trimmed, decimal_begin, last_relevant_decimal);
+
+	return trimmed;
+}
+
+
+
 Type::Type()
 {
 	is_null_ = true;
 }
 
 
-static std::string trim_irrelevant_str_double_zeros(const std::string& strdouble) noexcept
-{
-	std::string trimmed = strdouble;
-
-	size_t decimal_begin = 0;
-	size_t last_relevant_decimal = 0;
-
-	while (decimal_begin < trimmed.size() && trimmed[decimal_begin] != '.')
-		decimal_begin++;
-
-	last_relevant_decimal = decimal_begin;
-
-	for (size_t i = decimal_begin; i < trimmed.size(); i++)
-		if (trimmed[i] != '0')
-			last_relevant_decimal = i;
-
-	if (last_relevant_decimal == decimal_begin)
-		trimmed.erase(last_relevant_decimal);
-
-	else
-		trimmed.erase(last_relevant_decimal + 1);
-
-	return trimmed;
-}
-
-
 std::string type_to_string(const Type& type) noexcept
 {
-	switch (type.type_index())
+	switch (type.type())
 	{
 	case TypeIndex::Null: 	return "null";
 	
 	case TypeIndex::String: return type.as_str();
-	case TypeIndex::Number: return trim_irrelevant_str_double_zeros(std::to_string(type.as_num()));
+	case TypeIndex::Number: return trim_irrelevant_doublestr_zeros(std::to_string(type.as_num()));
 	case TypeIndex::Bool:	return bool_to_string(type.as_bool());
 	}
 
--- src/type/type.h
+++ src/type/type.h
@@ -22,9 +22,11 @@ using VariantType = std::variant<std::string, double, bool>;
 class Type;
 
 
+// returns a representation of a Type
 std::string type_to_string(const Type& type) noexcept;
 
 
+// language types
 class Type : public VariantType
 {
 public:
@@ -33,7 +35,8 @@ public:
 	Type();
 
 
-	TypeIndex type_index() const noexcept { return is_null() ? TypeIndex::Null : (TypeIndex)index(); }
+	// returns an enum that represents the current active type
+	TypeIndex type() const noexcept { return is_null() ? TypeIndex::Null : (TypeIndex)index(); }
 
 
 	std::string to_string() const noexcept { return type_to_string(*this); }
