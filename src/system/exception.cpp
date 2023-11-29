1019fc9a20cfd22c08f64e98a33ad9c8b2d27acf
Marvin Marjan <marvin.marjan@gmail.com>
2023-11-29 10:34:45 -0400

    splited exception functions into small pieces and add comments.

--- src/system/exception.cpp
+++ src/system/exception.cpp
@@ -8,35 +8,27 @@
 
 
 
-static std::string get_error_line(std::string& line, const TokenPosition& pos) noexcept
+static std::string generate_error_indicator(const std::string& line, const TokenPosition& pos)
 {
-	const std::string tabs = "\t";
-
-	std::stringstream stream;
 	std::string error_indicator = "";
 
-	while (line.front() == ' ' || line.front() == '\t')
-		line.erase(line.begin());
-
-	line.insert(pos.end, sp::RESET_ALL);
-	line.insert(pos.begin, sp::clr(sp::fg_bred));
-
+	// generates it
 	for (size_t i = 0; i < line.size() && i < pos.end; i++)
 	{
+		// before the begin of the token
 		if (i < pos.begin)
 			error_indicator += ' ';
 
+		// at the begin of the token
 		else if (i == pos.begin)
 			error_indicator += '^';
 
+		// after the begin of the token
 		else if (i > pos.begin)
 			error_indicator += '~';
 	}
 
-	stream << tabs << line << std::endl;
-	stream << tabs << error_indicator;
-
-	return stream.str();
+	return sp::bred(error_indicator);
 }
 
 
@@ -51,25 +43,55 @@ static std::string format_token_position(const TokenPosition& pos) noexcept
 }
 
 
+static std::string get_error_line(std::string& line, const TokenPosition& pos) noexcept
+{
+	// amount of tabs at the line begin
+	const std::string tabs = "\t";
+
+	std::stringstream stream;
+
+	// erases all blank charactes at the line begin
+	while (line.front() == ' ' || line.front() == '\t')
+		line.erase(line.begin());
+
+	// paints the error token with red
+	line.insert(pos.end, sp::RESET_ALL);
+	line.insert(pos.begin, sp::clr(sp::fg_bred));
+
+	stream << tabs << line << std::endl;
+	stream << tabs << generate_error_indicator(line, pos);
+
+	return stream.str();
+}
+
+
 
 std::string exception_to_string(const Exception& exception) noexcept
 {
 	const SystemState& state = sys_state();
 
+	// error position (generally a token)
 	const TokenPosition& pos = exception.pos;
 
+	// line where the error occurred
 	std::string line = state.vecsource[pos.line];
 	std::stringstream stream;
 
-	stream << sp::bred("Error ") << state.source_name;
+	// source where the error occurred
+	stream << std::endl << sp::bred("Error ") << state.source_name;
+
+	// error position
 	stream << sp::clr(sp::bold) << " (" << format_token_position(pos) << "): " << sp::RESET_ALL;
 
+	// error message
 	stream << exception.msg;
 
+	// error view
 	stream << std::endl << std::endl << get_error_line(line, pos) << std::endl << std::endl;
 
 	return stream.str();
 }
+
 
 
 
--- src/system/exception.h
+++ src/system/exception.h
@@ -15,8 +15,9 @@ struct Exception
 };
 
 
-
+// creates a exception message based on Exception object
 std::string exception_to_string(const Exception& exception) noexcept;
 
 
+// logs an error to the console
 void log_error(const Exception& exception) noexcept;
