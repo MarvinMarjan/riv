d8b0167f70b21fb578e467cd1b281fb62eab0335
Marvin Marjan <marvin.marjan@gmail.com>
2023-11-29 10:14:18 -0400

    improve exception message.

--- src/system/exception.cpp
+++ src/system/exception.cpp
@@ -2,7 +2,53 @@
 
 #include <sstream>
 
+#include <specter/output/color/color.h>
+
 #include <system/sysstate.h>
+
+
+
+static std::string get_error_line(std::string& line, const TokenPosition& pos) noexcept
+{
+	const std::string tabs = "\t";
+
+	std::stringstream stream;
+	std::string error_indicator = "";
+
+	while (line.front() == ' ' || line.front() == '\t')
+		line.erase(line.begin());
+
+	line.insert(pos.end, sp::RESET_ALL);
+	line.insert(pos.begin, sp::clr(sp::fg_bred));
+
+	for (size_t i = 0; i < line.size() && i < pos.end; i++)
+	{
+		if (i < pos.begin)
+			error_indicator += ' ';
+
+		else if (i == pos.begin)
+			error_indicator += '^';
+
+		else if (i > pos.begin)
+			error_indicator += '~';
+	}
+
+	stream << tabs << line << std::endl;
+	stream << tabs << error_indicator;
+
+	return stream.str();
+}
+
+
+static std::string format_token_position(const TokenPosition& pos) noexcept
+{
+	std::stringstream stream;
+
+	stream << "line " << pos.line + 1 << ", ";
+	stream << "col " << pos.begin << ":" << pos.end;
+
+	return stream.str();
+}
 
 
 
@@ -12,10 +58,15 @@ std::string exception_to_string(const Exception& exception) noexcept
 
 	const TokenPosition& pos = exception.pos;
 
+	std::string line = state.vecsource[pos.line];
 	std::stringstream stream;
 
-	stream << "Error " << state.source_name << " (" << "line " << pos.line + 1 << ", col " << pos.begin << ":" << pos.end << "): ";
+	stream << sp::bred("Error ") << state.source_name;
+	stream << sp::clr(sp::bold) << " (" << format_token_position(pos) << "): " << sp::RESET_ALL;
+
 	stream << exception.msg;
+
+	stream << std::endl << std::endl << get_error_line(line, pos) << std::endl << std::endl;
 
 	return stream.str();
 }
